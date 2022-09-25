0423daf625f309aeb93e153368c69e35ebe7bf71
Julien V <vier.julien@googlemail.com>
2022-09-25 18:23:09 +0200

    Mood logic update

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 
 
-// 1. Toggle active class after clicking on one of the images
+// 1. Toggle active class after clicking on one of the images. Can't click on more than 1 mood.
 
-// const cardClick = document.getElementById("card-happy");
 const cards = document.getElementsByClassName("card");
-console.log(cards);
+const activeCards = document.getElementsByClassName("active");
+
+const message = document.querySelector(".message h3");
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
-        cards[i].classList.add("active");
-    });  
+        if (activeCards.length === 0) {
+            cards[i].classList.add("active");
+            message.textContent = "Nice one! See you tomorrow!"; // Customise depending on mood picked
+            message.style.visibility = "visible";
+        } else {
+            alert("You've already picked a mood for today! Come back tomorrow");
+        }
+    });
 };
 
 
 
 
+// 1. Replace alert() with a modal
+ 
 // 2. Display message after picking mood / customise it depending on mood selected
 
 // 3. Timing
-//      3.a. only let people pick one mood per day - add to localStorage
-//      3.b. update page every day - Date() object
+//      3.a. only let people pick one mood per day
+//      3.b. add to localStorage
+//      3.c. update page every day - Date() object
 
 // 4. Analytics:
 //      3.a. count clicks on different moods to calculate average
